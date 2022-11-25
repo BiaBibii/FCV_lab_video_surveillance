9a0508001687a842059ccc9e3c1e4f8ca8fec46e
BiaBibii <beuka_bianka@yahoo.com>
2022-11-25 07:49:00 +0200

    final with gamma correction

--- main.py
+++ main.py
@@ -1,48 +1,58 @@
 import cv2
 import numpy as np
+
 cap = cv2.VideoCapture(0)
 last_mean = 0
-detected_motion = False
 frame_rec_count = 0
+frame_rec_count1 = 0
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('output.avi', fourcc, 10.0, (640, 480))
 
 ret_init, frame_init = cap.read()
-frame_number = 4
-while (cap.isOpened()):
-    if frame_rec_count == 0:
-        ret, prev_frame = cap.read()
-        if ret == True:
-            prev = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
-    detected_motion = False
+summ=0
+diff_fram=[]
+def gammaCorrection(src, gamma):
+    invGamma = 1 / gamma
+
+    table = [((i / 255) ** invGamma) * 255 for i in range(256)]
+    table = np.array(table, np.uint8)
+
+    return cv2.LUT(src, table)
+
+while (True):
     ret, frame = cap.read()
-    if ret:
-        frame_rec_count = frame_rec_count + 1
-        orig_frame = frame.copy()
+    ret1, frame1 = cap.read()
+    x=gammaCorrection(frame, 2.5)
+    # cv2.imshow("ij",x)
+    if frame_rec_count == 0:
+        gray = cv2.cvtColor(frame_init, cv2.COLOR_BGR2GRAY)
+        gammaCorrection(frame_init,2.5)
+        first_mean = np.mean(gray)
+        result = np.abs(np.mean(gray) - first_mean)
+        # print("first res", result)
+        last_mean = np.mean(gray)
+
+
+    else:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        if frame_rec_count % frame_number == 0 or frame_rec_count == 1:
-            frame_list = []
-        dif_frames = cv2.absdiff(gray, prev)
-        # print(dif_frames)
-        ret, threshold = cv2.threshold(dif_frames, 80, 255, cv2.THRESH_BINARY)
-        # print(threshold)
+        # diff_fram= np.append(diff_fram,frame)
+        result = np.abs(np.mean(gray) - last_mean)
+        last_mean = np.mean(gray)
 
-        dilate_frame = cv2.dilate(threshold, None, iterations=2)
-        prev = gray
-        # print(threshold)
-        # print(dif_frames)
-        dif_frames=np.append(dif_frames,dilate_frame)
-        if len(frame_list) == frame_number:
-            sum_fr = sum(frame_list)
 
-            cv2.line(orig_frame, (0, 0), (480, 640), (0, 0, 255), 34)
-            print(sum)
+    frame_rec_count = frame_rec_count + 1
+    if result > 0.3:
+        out.write(frame)
+        frame_rec_count = frame_rec_count + 1
+    else:
+        cv2.line(frame, (0, 0), (480, 640), (0, 0, 255), 34)
+        frame_rec_count1 = frame_rec_count1 + 1
 
-            cv2.imshow('frame1', orig_frame)
-            if (cv2.waitKey(1) & 0xFF == ord('q')):
-                 break
+    cv2.imshow('frame1', frame)
+    if (cv2.waitKey(1) & 0xFF == ord('q')):
+        break
 
 cap.release()
 cv2.destroyAllWindows()
 print(frame_rec_count)
-# print(frame_rec_count1)
+print(frame_rec_count1)
